8fc10df195514369d406a1e09746e7938a028d6d
Tanvir Ahmed <tanvirahmedmd994@gmail.com>
2026-02-24 15:26:07 -0600

    make the jwt token system in this project

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 const express = require("express");
 const cors = require("cors");
 // environment secure user and pass
-require('dotenv').config()
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
+
+// firebase service account code
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./smart-deals-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-  const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qqlveqa.mongodb.net/?appName=Cluster0`;
+// concept of middleware jwt token
+const logger = (req, res, next) => {
+  console.log("user information");
+  next();
+};
+
+const verifyFirebaseToken = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return req.status(401).send({ message: "unauthorized token" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return req.status(401).send({ message: "unauthorized token" });
+  }
+
+  // verify token
+  try {
+    const userInfo = await admin.auth().verifyIdToken(token);
+    req.token_email = userInfo.email;
+    console.log("after token validation", userInfo);
+    // call the next
+    next();
+  } catch {
+    return req.status(401).send({ message: "unauthorized token" });
+  }
+};
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qqlveqa.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,7 +65,7 @@ app.get("/", (req, res) => {
 // database connect user
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("smart_db");
     const productsCollection = db.collection("products");
@@ -43,7 +80,7 @@ async function run() {
       const query = { email: email };
       const existingUser = await usersCollection.findOne(query);
       if (existingUser) {
-        res.send({massage: 'user already exist.'});
+        res.send({ massage: "user already exist." });
       } else {
         const result = await usersCollection.insertOne(newUser);
         res.send(result);
@@ -71,15 +108,18 @@ async function run() {
     });
 
     // sob product paite chaile
-    app.get('/latest-products', async (req, res) => {
-      const cursor = productsCollection.find().sort({ created_at: -1 }).limit(6);
+    app.get("/latest-products", async (req, res) => {
+      const cursor = productsCollection
+        .find()
+        .sort({ created_at: -1 })
+        .limit(6);
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: (id) };
+      const query = { _id: id };
       const result = await productsCollection.findOne(query);
       res.send(result);
     });
@@ -112,12 +152,16 @@ async function run() {
       res.send(result);
     });
 
-
     // bids related Apis
-    app.get("/bids", async (req, res) => {
+    app.get("/bids", logger, verifyFirebaseToken, async (req, res) => {
+      console.log("headers", req);
       const email = req.query.email;
       const query = {};
+
       if (email) {
+        if (email !== req.token_email) {
+          return res.status(403).send({ massage: "Forbidden access" });
+        }
         query.buyer_email = email;
       }
 
@@ -126,13 +170,17 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/products/bids/:productId', async (req, res) => {
-      const productId = req.params.productId;
-      const query = { product: productId }
-      const cursor = bidsCollection.find(query).sort({bid_price: -1})
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+    app.get(
+      "/products/bids/:productId",
+      verifyFirebaseToken,
+      async (req, res) => {
+        const productId = req.params.productId;
+        const query = { product: productId };
+        const cursor = bidsCollection.find(query).sort({ bid_price: -1 });
+        const result = await cursor.toArray();
+        res.send(result);
+      },
+    );
 
     app.get("/bids/:id", async (req, res) => {
       const id = req.params.id;
@@ -142,17 +190,16 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/bids', async (req, res) => {
+    app.get("/bids", async (req, res) => {
       const query = {};
       if (query.email) {
-        query.buyer_email = email
+        query.buyer_email = email;
       }
 
-
       const cursor = bidsCollection.find(query);
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.post("/bids", async (req, res) => {
       const newBids = req.body;
@@ -167,7 +214,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
